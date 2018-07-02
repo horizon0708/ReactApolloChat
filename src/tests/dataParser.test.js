d14a395dd810c3b098b3c3aa10062ec9523c5f21
James Kim <james.kim.nz92@gmail.com>
2018-07-03 10:40:01 +1200

    mvp 2.1

--- src/tests/dataParser.test.js
+++
@@ -1,9 +0,0 @@
-import { parseDate } from '../utility/dateHelper'
-
-const past = "2018-06-19 09:17:30.231000";
-
-it('parses date', () => {
-  const res = parseDate(past)
-  console.log(res);
-  // expect(res).toBe('') 
-});
